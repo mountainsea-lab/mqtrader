3228bb1f108ba87cb8047e3319696a244bc888a4
mountainsea-lab <w1999wtw3537@gmail.com>
2026-04-11 19:06:57 +0800

    use jemallocator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
 use mqtrader::nautilus::bot::mqtrader::MqTrader;
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 #[tokio::main]
 async fn main() -> Result<()> {
